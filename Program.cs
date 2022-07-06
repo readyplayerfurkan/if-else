e719c6484473b4937462e2fce098bef36aa16700
fullstackfurkan <furkanyilmaz870@gmail.com>
2022-07-06 12:12:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+int time = DateTime.Now.Hour;
+
+if ((time >= 6) && (time < 11))
+    Console.WriteLine("Günaydın!");
+else if (time <= 18)
+    Console.WriteLine("İyi Günler!");
+else
+    Console.WriteLine("İyi Akşamlar!");
+
+
+int kilo = Convert.ToInt32(Console.ReadLine());
+
+string saglik = kilo >= 100 ? "Sağlığına dikkat et!" : "Sağlıklısın, böyle devam et!";
+
+Console.WriteLine(saglik);
+
+
+
+
+
+
+Console.ReadLine();
+
